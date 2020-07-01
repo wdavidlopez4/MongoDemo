f7a99cc84b98e2eb3f96dca7d7783de3b7cb1eb4
David Lopez <wdavidlopez4@gmail.com>
2020-07-01 12:01:09 -0500

    Creacion de Interfaces de la capa de Domain

---
+++ src/Domain/Interfaces/IModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IModels<T>
+    {
+        public T Get(string id);
+
+        public IEnumerable<T> Get();
+
+        public T Create(T objet);
+
+        public bool Delete(T objet);
+
+        public bool Update(T objet);
+    }
+}
