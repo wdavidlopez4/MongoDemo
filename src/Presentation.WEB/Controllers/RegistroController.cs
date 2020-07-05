f41359efa1874bf088055f7f9ed72c51a66361b5
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-07-05 17:41:19 -0500

    Creacion del primero controlador RegistroControler

---
+++ src/Presentation.WEB/Controllers/RegistroController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.WEB.ViewModel;
+
+namespace Presentation.WEB.Controllers
+{
+    public class RegistroController : Controller
+    {
+        private readonly IServices service;
+
+        public RegistroController(IServices service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearRegistro([Bind("Cliente")] RegistroVM registroVM)
+        {
+            var cliente = registroVM.Cliente;
+            var seGuardo = service.Guardar(cliente);
+
+            if(seGuardo != null)
+            {
+                RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+    }
+}
